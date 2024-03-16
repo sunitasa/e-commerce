3d38896d59d07b4da8982ac58aae7735672a012a
Sunita <sunita859688@gmail.com>
2024-03-17 00:22:47 +0545

    topbar component completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import 'bootstrap/dist/css/bootstrap.css';
+// import Layout from "./Layout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Home from "./components/Home";
 export default function App() {
     return (
--- src/components/common/Topbar.js
+++ src/components/common/Topbar.js
@@ -2,35 +2,44 @@ import React from "react";
 import { NavLink } from 'react-router-dom'
 export default function Topbar() {
     return (
-
-        <div className="container">
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <img src="./location-icon.svg" class="img-fluid" alt=" location" />
-                <span>Store Location: Lincoln- 344, Illinois, Chicago, USA</span>
-                <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Eng
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">USD
-                    </a>
-                    <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        <li class="nav-item">
-                            <a className="nav-link" href="#">Sign Up</a>
+        <section className="bg-dark text-secondary">
+            <div className="container">
+                <nav className="navbar py-0 navbar-expand-sm">
+                    <span className="navbar-brand py-0 d-flex justify-content-center" href="#">
+                        <img src="/images/location-icon.svg" class="img-fluid" alt=" location" />
+                        <span className="text-secondary ps-2 font-small">Store Location: Lincoln- 344, Illinois, Chicago, USA</span>
+                    </span>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link text-secondary dropdown-toggle" href="#" id="langToggler" role="button" data-bs-toggle="dropdown" aria-expanded="false">Eng
+                            </a>
+                            <ul className="dropdown-menu" id="lang-dropdown" aria-labelledby="lang-dropdown">
+                                <li><a className="dropdown-item" href="#">ENG</a></li>
+                                <li><a className="dropdown-item" href="#">NEP</a></li>
+                                <li><a className="dropdown-item" href="#">SPA</a></li>
+                            </ul>
                         </li>
-                        <li classN="nav-item">
-                            <a class="nav-link" href="#">/Sign In</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link text-secondary dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">USD
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="currencyDropdown">
+                                <li><a className="dropdown-item" href="#">USD</a></li>
+                                <li><a className="dropdown-item" href="#">NPR</a></li>
+                                <li><a className="dropdown-item" href="#">JPY</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a className="nav-link text-secondary" href="#">Sign Up</a>
+                        </li>
+                        <span className="navbar-text text-secondary">/</span>
+                        <li className="nav-item">
+                            <a class="nav-link text-secondary" href="#">Sign In</a>
                         </li>
                     </ul>
-                </li>
-            </nav>
-        </div>
+                </nav>
+            </div>
+        </section>
     );
 
 };
