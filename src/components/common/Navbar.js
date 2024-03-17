faaf71e031213b086e60b1babb83eb7ec1b41305
Sunita <sunita859688@gmail.com>
2024-03-17 11:50:07 +0545

    header area completed

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -2,38 +2,78 @@ import React from "react";
 import { NavLink } from 'react-router-dom'
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+    
+    <section className="bg-body-tertiary">
+      <div className="container">
+        <nav className="navbar p-0 navbar-expand-lg font-small">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <a className="nav-link nav-highlighted dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <span class="navbar-toggler-icon"></span> All Categories
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">category-1</a></li>
+                  <li><a className="dropdown-item" href="#">category-2</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">category-3</a></li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Home
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Home-1</a></li>
+                  <li><a className="dropdown-item" href="#">Home-2</a></li>
+                  <li><a className="dropdown-item" href="#">Home-3</a></li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Shop
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Shop-1</a></li>
+                  <li><a className="dropdown-item" href="#">Shop-2</a></li>
+                  <li><a className="dropdown-item" href="#">Shop-3</a></li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Pages
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Pages-1</a></li>
+                  <li><a className="dropdown-item" href="#">Pages-2</a></li>
+                  <li><a className="dropdown-item" href="#">Pages-3</a></li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Blog
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Blog-1</a></li>
+                  <li><a className="dropdown-item" href="#">Blog-2</a></li>
+                  <li><a className="dropdown-item" href="#">Blog-3</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About Us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <img className="me-1" src="/images/phone-icon.svg" alt="phone" />
+          (219) 555-0114
+        </nav>
+      </div>
+    </section>
   );
 };
--- src/components/common/SiteHeader.js
+++ src/components/common/SiteHeader.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function SiteHeader() {
   return (
-    <section className=''>
+    <section className='py-2'>
       <div className="container">
         <nav className="navbar">
           <a className="navbar-brand">
-            <img src='/images/brand-icon.png' alt='brand icon' />
-            Ecobazar
+            <img className='me-2' style={{height:'28px'}} src='/images/brand-icon.svg' alt='brand icon' />
+            <span className='fw-bold color-dark fs-5 lh-base'>Ecobazar</span>
           </a>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
@@ -17,9 +17,9 @@ export default function SiteHeader() {
             <img className='header-icon' src='/images/favorite.svg' alt='favorite' />
             <div className='vr-separator m-0 ms-2 me-2'></div>
             <img className='header-icon me-2' src='/images/bag.svg' alt='bag' />
-            <div className='font-small lh-sm'>
-              <p className='m-0 text-secondary'>Shopping cart:</p>
-              <p className='m-0 fw-bold'>$57.00</p>
+            <div className='lh-sm'>
+              <p className='m-0 font-smaller text-secondary'>Shopping cart:</p>
+              <p className='m-0 font-small fw-bold'>$57.00</p>
             </div>
           </div>
         </nav>
