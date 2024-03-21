59df3cd2121a8801c06daf339fdbc651ff741256
Sunita <sunita859688@gmail.com>
2024-03-21 11:10:11 +0545

    testimonial images added

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <section id='footer' className='footer-up'>
+    <section id='footer' className='footer-up pt-5 pb-5'>
       <div className='container pt-5'>
-        <div className='row pt-5'>
-          <div className='col-md-4 pt-5'>
-          <a className="navbar-brand">
-            <img className='me-2' style={{height:'28px'}} src='/images/brand-icon.svg' alt='brand icon' />
-            <span className='fw-bold color-dark fs-5 lh-base'>Ecobazar</span>
-          </a>
+        <div className='row mt-5'>
+          <div className='col-md-4'>
+            <a className="navbar-brand">
+              <img className='me-2' style={{height:'28px'}} src='/images/brand-icon.svg' alt='brand icon' />
+              <span className='fw-bold color-dark fs-5 lh-base'>Ecobazar</span>
+            </a>
           </div>
-          <div className='col-md-4 pt-5'>
-          <h6>Subcribe our Newsletter</h6>
-          <p>Pellentesque eu nibh eget mauris congue mattis matti.</p>
+          <div className='col-md-4'>
+            <h6>Subcribe our Newsletter</h6>
+            <p>Pellentesque eu nibh eget mauris congue mattis matti.</p>
           </div>
-          <div className='col-md-4 pt-5'>
-          <div class="bg-light rounded-pill my-3 d-flex">
-              <input type="text" class=" border-0 rounded-pill pe-5" placeholder="Your Email" />
+          <div className='col-md-4'>
+            <div class="bg-white rounded-pill d-flex">
+              <input type="text" class="border-0 flex-grow-1 rounded-pill px-4 outline-0" placeholder="Your Email" />
               <button class="btn btn-success rounded-pill align-self-end px-5">Subscribe</button>
             </div>
           </div>
--- src/components/home/Testimonials.js
+++ src/components/home/Testimonials.js
@@ -16,20 +16,20 @@ export default function Testimonials() {
         <div className='col-md-4 text-center p-3'>
         <img className='img-fluid' src='/images/leaf.svg' alt='leaf' />
           <p>“Aenean et nisl eget eros consectetur vestibulum vel id erat. Aliquam feugiat massa dui. Sed sagittis diam sit amet ante sodales semper. Aliquam commodo lorem laoreet ultricies ele. ”</p>
-          <img className='img-fluid' src='/images/1stimg.png' alt='person img' />
-          <h5>Jenny Wilson</h5>
+          <img className='img-fluid' src='/images/3rdimage.png' alt='person img' />
+          <h5>Kathryn Murphy</h5>
           <p>Customer</p>
         </div>
         <div className='col-md-4 text-center p-3'>
         <img className='img-fluid' src='/images/leaf.svg' alt='leaf' />
           <p>“Aenean et nisl eget eros consectetur vestibulum vel id erat. Aliquam feugiat massa dui. Sed sagittis diam sit amet ante sodales semper. Aliquam commodo lorem laoreet ultricies ele. ”</p>
-          <img className='img-fluid' src='/images/1stimg.png' alt='person img' />
-          <h5>Jenny Wilson</h5>
+          <img className='img-fluid' src='/images/2ndimage.png' alt='person img' />
+          <h5>Guy Hawkins</h5>
           <p>Customer</p>
         </div>
       </div>
       <div className='video text-center negative-margin pt-5 mt-5'>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Jhb471IhydE" title="YouTube video player"
+      <iframe width="560" height="315" src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen="false">
